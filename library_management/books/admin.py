bbe0270f648bad41fce12a87a61fe838118af102
luckyasu <137876644+luckyasu@users.noreply.github.com>
2024-06-07 11:30:52 +0800

    Delete library_management directory

--- library_management/books/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Author, Book, Manage, Publisher
-
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Manage)
-admin.site.register(Publisher)
